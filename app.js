2c66a88c7e44e80dc8060a83503b4806d69c563a
Muhammad Tayyab <124426676+Tayyab-Khanzada@users.noreply.github.com>
2023-09-25 06:10:11 -0700

    changes added

--- app.js
+++ app.js
@@ -1,34 +1,7 @@
-let locase1 = "I'm Muhammad Tayyab "
+let para = document.querySelector("#para").addEventListener('click', abc);
+function abc(){
+    // let color =   prompt("Enter your Color.")
+    //  let color = ["orange", "yellow","aqua", "green", "pink","gray"]
+    document.querySelector("#para").body.style.background = "green";
 
-let convert = locase1.toUpperCase();
-
-function loCase(){
-
-    document.getElementById("para").innerHTML =  convert;
 }
-function gstCalc(){}
-
-    
-    let cost =+ document.getElementById("text").value;
-    
-    if(!cost){
-        alert("Please Enter a Tax ")
-        return;
-    }
-    
-    cost = Number(cost)
-    let taxInput =+ prompt("Entere Tax Value")
-    
-    let tax = cost * (taxInput /100)
-    let totalCost = tax + cost
-
-    
-    totalCost =  Math.round(totalCost)
-    document.getElementById("btn1").innerHTML = totalCost;
-// }
-
-
-let date = new Date().getFullYear()
-console.log(date)
-
-document.getElementById("year").innerHTML = date;
